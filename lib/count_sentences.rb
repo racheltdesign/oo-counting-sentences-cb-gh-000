d59e36180d2b8ef8b2e3768fdacbab60c3829be0
Rachel Thompson <thompr@pba.edu>
2017-08-11 01:40:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+     self.split(/[.?!]/).delete_if{|s| s.strip == ''}.count
   end
+
 end
